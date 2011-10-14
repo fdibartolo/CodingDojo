09c33a878249eb131c5fad462e7e520ed252419f
Fernando Di Bartolo <fernando.di.bartolo@gmail.com>
2011-10-14 14:48:21 -0300

    Adding files, first empty commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TrainingPortal::Application.config.secret_token = 'a013131c1892703e4f502a9b8591019ad851d1e9a734da80bfedee3183529308783e1915a91c6c2417d4d2798a3e5c7b073b736ba8c4367f3efef8d100b2d02e'
