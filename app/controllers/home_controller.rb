40bdbd5644309f008f45a701226df3680878e175
Fernando Di Bartolo <fernando.di.bartolo@gmail.com>
2011-10-26 19:50:41 -0300

    handle open sessions if any at home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,17 @@
 class HomeController < ApplicationController
   def index
-    @open_session = Session.where(:status => "Open").first!
+    open_sessions = Session.where(:status => "Open")
+
+    if open_sessions.count > 0
+      @open_session = open_sessions.first!
+      @has = true
+    else
+      @has = false
+    end
 
     respond_to do |format|
       format.html # index.html.erb
-      format.json { render json: @session }
+      format.json { render json: @open_session }
     end
   end
 end
