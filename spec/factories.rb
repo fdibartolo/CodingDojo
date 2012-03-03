88b558583ecdc5f7cc162b7510b1c3df8a7ee1ec
Fernando Di Bartolo <fernando.di.bartolo@gmail.com>
2012-03-03 19:03:17 -0300

    add home page tests

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,6 +1,8 @@
 FactoryGirl.define do
   factory :session do
-    date      Date.today
-    status    "Open"
+    date        Date.today
+    start_time  Time.now
+    end_time    Time.now + 1.hour
+    status      "Open"
   end
 end
--- spec/requests/home_pages_spec.rb
+++ spec/requests/home_pages_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
 describe "Home pages" do
-  
+  describe "index view" do
+    it "should display info about upcoming open session" do
+      FactoryGirl.create(:session)
+      visit root_path
+      page.should have_selector('h1', text: 'next session')
+    end
+
+    it "should display 'stay tunned' when no sessions are open" do
+      FactoryGirl.create(:session, :status => "Closed")
+      visit root_path
+      page.should have_selector('h1', text: 'no open sessions yet, stay tuned!')
+    end    
+  end
 end
